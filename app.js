75d6cbfb11a2debc736d77fa3c94c16dc8321535
DARCY MCCABE <DARMCCA@GMAIL.COM>
2016-12-13 08:57:10 -0800

    basic pseudo code outline for js

---
+++ app.js
@@ -0,0 +1,16 @@
+// array of all images
+// empty array to push images to
+
+//Object
+//Not sure which properties to put here: views & clicks
+
+// number of tries determined by for loop
+//for(i=25; i <=0; i--);
+//remove listener- after 25 tries
+
+// if statement about previous views so that the same views
+//don't happen twice in a row
+
+//logic to render/create random images
+
+//event handler logic 
